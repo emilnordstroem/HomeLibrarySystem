25287848e66b1ea3702ecaf3ea25517164045b1d
emilnordstroem <emilstoeve@outlook.com>
2024-11-13 17:19:47 +0100

    Constructed Library.java and Address.java

--- src/application/model/library/Library.java
+++ src/application/model/library/Library.java
@@ -1,4 +1,25 @@
 package application.model.library;
 
+import application.model.utilities.Address;
+
+import java.util.ArrayList;
+
 public class Library {
+    private Address address;
+    private ArrayList<Bookcase> bookcaseList = new ArrayList<>();
+
+    public Library(Address address){
+        this.address = address;
+    }
+
+    public void addBookcase(Bookcase bookcase){
+        if(!bookcaseList.contains(bookcase)){
+            bookcaseList.add(bookcase);
+        }
+    }
+
+    // Incomplete
+    public Bookcase getBookcase(Bookcase bookcase){
+        return null;
+    }
 }
--- src/application/model/utilities/Address.java
+++ src/application/model/utilities/Address.java
@@ -1,4 +1,13 @@
 package application.model.utilities;
 
 public class Address {
+    private String streetName;
+    private String buildingNo;
+    private String postalCode;
+
+    public Address(String streetName, String buildingNo, String postalCode){
+        this.streetName = streetName;
+        this.buildingNo = buildingNo;
+        this.postalCode = postalCode;
+    }
 }
